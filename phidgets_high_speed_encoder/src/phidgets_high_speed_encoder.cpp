cb616b5c1641cbe0933bb23f6356d0f79ebac817
Geoff Viola <geoffrey.viola@asirobots.com>
2016-05-27 11:01:47 -0600

    reformatted and added print at startup

--- phidgets_high_speed_encoder/src/phidgets_high_speed_encoder.cpp
+++ phidgets_high_speed_encoder/src/phidgets_high_speed_encoder.cpp
@@ -46,144 +46,146 @@ static bool inverted = false;
 
 int AttachHandler(CPhidgetHandle phid, void *userptr)
 {
-    int serial_number;
-    const char *name;
+  int serial_number;
+  const char *name;
 
-    CPhidget_getDeviceName (phid, &name);
-    CPhidget_getSerialNumber(phid, &serial_number);
-    ROS_INFO("%s Serial number %d attached!",
-			 name, serial_number);
+  CPhidget_getDeviceName(phid, &name);
+  CPhidget_getSerialNumber(phid, &serial_number);
+  ROS_INFO("%s Serial number %d attached!",
+           name, serial_number);
 
-    return 0;
+  return 0;
 }
 
 int DetachHandler(CPhidgetHandle phid, void *userptr)
 {
-    int serial_number;
-    const char *name;
+  int serial_number;
+  const char *name;
 
-    CPhidget_getDeviceName (phid, &name);
-    CPhidget_getSerialNumber(phid, &serial_number);
-    ROS_INFO("%s Serial number %d detached!",
-			 name, serial_number);
+  CPhidget_getDeviceName(phid, &name);
+  CPhidget_getSerialNumber(phid, &serial_number);
+  ROS_INFO("%s Serial number %d detached!",
+           name, serial_number);
 
-    return 0;
+  return 0;
 }
 
 int ErrorHandler(CPhidgetHandle phid, void *userptr,
-				 int ErrorCode, const char *Description)
+                 int ErrorCode, const char *Description)
 {
-    ROS_INFO("Error handled. %d - %s", ErrorCode, Description);
-    return 0;
+  ROS_INFO("Error handled. %d - %s", ErrorCode, Description);
+  return 0;
 }
 
 int InputChangeHandler(CPhidgetEncoderHandle ENC,
-					   void *usrptr, int Index, int State)
+                       void *usrptr, int Index, int State)
 {
-    return 0;
+  return 0;
 }
 
 int PositionChangeHandler(CPhidgetEncoderHandle ENC,
-						  void *usrptr, int Index,
-						  int Time, int RelativePosition)
+                          void *usrptr, int Index,
+                          int Time, int RelativePosition)
 {
-    static uint32_t sequence_number = 0U;
-    int Position;
-    CPhidgetEncoder_getPosition(ENC, Index, &Position);
+  static uint32_t sequence_number = 0U;
+  int Position;
+  CPhidgetEncoder_getPosition(ENC, Index, &Position);
 
-    phidgets_high_speed_encoder::encoder_params e;
-    e.header.stamp = ros::Time::now();
-    e.header.frame_id = frame_id;
-    e.header.seq = sequence_number++;
-    e.index = Index;
-    e.count = (inverted ? -Position : Position);
-    e.count_change = (inverted ? -RelativePosition : RelativePosition);
-    e.time = Time;
-    if (initialised) encoder_pub.publish(e);
-    ROS_DEBUG("Encoder %d Count %d", Index, Position);
-    return 0;
+  phidgets_high_speed_encoder::encoder_params e;
+  e.header.stamp = ros::Time::now();
+  e.header.frame_id = frame_id;
+  e.header.seq = sequence_number++;
+  e.index = Index;
+  e.count = (inverted ? -Position : Position);
+  e.count_change = (inverted ? -RelativePosition : RelativePosition);
+  e.time = Time;
+  if (initialised) encoder_pub.publish(e);
+  ROS_DEBUG("Encoder %d Count %d", Index, Position);
+  return 0;
 }
 
 int display_properties(CPhidgetEncoderHandle phid)
 {
-    int serial_number, version, num_encoders, num_inputs;
-    const char* ptr;
+  int serial_number, version, num_encoders, num_inputs;
+  const char *ptr;
 
-    CPhidget_getDeviceType((CPhidgetHandle)phid, &ptr);
-    CPhidget_getSerialNumber((CPhidgetHandle)phid,
-							 &serial_number);
-    CPhidget_getDeviceVersion((CPhidgetHandle)phid, &version);
+  CPhidget_getDeviceType((CPhidgetHandle) phid, &ptr);
+  CPhidget_getSerialNumber((CPhidgetHandle) phid,
+                           &serial_number);
+  CPhidget_getDeviceVersion((CPhidgetHandle) phid, &version);
 
-    CPhidgetEncoder_getInputCount(phid, &num_inputs);
-    CPhidgetEncoder_getEncoderCount(phid, &num_encoders);
+  CPhidgetEncoder_getInputCount(phid, &num_inputs);
+  CPhidgetEncoder_getEncoderCount(phid, &num_encoders);
 
-    ROS_INFO("%s", ptr);
-    ROS_INFO("Serial Number: %d", serial_number);
-    ROS_INFO("Version: %d", version);
-    ROS_INFO("Number of encoders %d", num_encoders);
+  ROS_INFO("%s", ptr);
+  ROS_INFO("Serial Number: %d", serial_number);
+  ROS_INFO("Version: %d", version);
+  ROS_INFO("Number of encoders %d", num_encoders);
 
-    return 0;
+  return 0;
 }
 
 bool attach(
-			CPhidgetEncoderHandle &phid,
-			int serial_number)
+    CPhidgetEncoderHandle &phid,
+    int serial_number)
 {
-    // create the object
-    CPhidgetEncoder_create(&phid);
+  // create the object
+  CPhidgetEncoder_create(&phid);
 
-    // Set the handlers to be run when the device is
-	// plugged in or opened from software, unplugged or
-	// closed from software, or generates an error.
-    CPhidget_set_OnAttach_Handler((CPhidgetHandle)phid,
-								  AttachHandler, NULL);
-    CPhidget_set_OnDetach_Handler((CPhidgetHandle)phid,
-								  DetachHandler, NULL);
-    CPhidget_set_OnError_Handler((CPhidgetHandle)phid,
-								 ErrorHandler, NULL);
+  // Set the handlers to be run when the device is
+  // plugged in or opened from software, unplugged or
+  // closed from software, or generates an error.
+  CPhidget_set_OnAttach_Handler((CPhidgetHandle) phid,
+                                AttachHandler, NULL);
+  CPhidget_set_OnDetach_Handler((CPhidgetHandle) phid,
+                                DetachHandler, NULL);
+  CPhidget_set_OnError_Handler((CPhidgetHandle) phid,
+                               ErrorHandler, NULL);
 
-    // Registers a callback that will run if an input changes.
-    // Requires the handle for the Phidget, the function
-	// that will be called, and an arbitrary pointer that
-	// will be supplied to the callback function (may be NULL).
-    CPhidgetEncoder_set_OnInputChange_Handler(phid,
-											  InputChangeHandler,
-											  NULL);
+  // Registers a callback that will run if an input changes.
+  // Requires the handle for the Phidget, the function
+  // that will be called, and an arbitrary pointer that
+  // will be supplied to the callback function (may be NULL).
+  CPhidgetEncoder_set_OnInputChange_Handler(phid,
+                                            InputChangeHandler,
+                                            NULL);
 
-    // Registers a callback that will run if the
-	// encoder changes.
-    // Requires the handle for the Encoder, the function
-	// that will be called, and an arbitrary pointer that
-	// will be supplied to the callback function (may be NULL).
-    CPhidgetEncoder_set_OnPositionChange_Handler (phid,
-												  PositionChangeHandler,
-												  NULL);
+  // Registers a callback that will run if the
+  // encoder changes.
+  // Requires the handle for the Encoder, the function
+  // that will be called, and an arbitrary pointer that
+  // will be supplied to the callback function (may be NULL).
+  CPhidgetEncoder_set_OnPositionChange_Handler(phid,
+                                               PositionChangeHandler,
+                                               NULL);
 
-    //open the device for connections
-    CPhidget_open((CPhidgetHandle)phid, serial_number);
+  //open the device for connections
+  CPhidget_open((CPhidgetHandle) phid, serial_number);
 
-    // get the program to wait for an encoder device
-	// to be attached
-    if (serial_number == -1) {
-        ROS_INFO("Waiting for High Speed Encoder Phidget " \
-				 "to be attached....");
-    }
-    else {
-        ROS_INFO("Waiting for High Speed Encoder Phidget " \
-				 "%d to be attached....", serial_number);
-    }
-    int result;
-    if((result =
-		CPhidget_waitForAttachment((CPhidgetHandle)phid,
-								   10000)))
-		{
-			const char *err;
-			CPhidget_getErrorDescription(result, &err);
-			ROS_ERROR("Problem waiting for attachment: %s",
-					  err);
-			return false;
-		}
-    else return true;
+  // get the program to wait for an encoder device
+  // to be attached
+  if (serial_number == -1)
+  {
+    ROS_INFO("Waiting for High Speed Encoder Phidget " \
+         "to be attached....");
+  }
+  else
+  {
+    ROS_INFO("Waiting for High Speed Encoder Phidget " \
+         "%d to be attached....", serial_number);
+  }
+  int result;
+  if ((result =
+           CPhidget_waitForAttachment((CPhidgetHandle) phid,
+                                      10000)))
+  {
+    const char *err;
+    CPhidget_getErrorDescription(result, &err);
+    ROS_ERROR("Problem waiting for attachment: %s",
+              err);
+    return false;
+  }
+  else return true;
 }
 
 /*!
@@ -191,50 +193,58 @@ bool attach(
  */
 void disconnect(CPhidgetEncoderHandle &phid)
 {
-    ROS_INFO("Closing...");
-    CPhidget_close((CPhidgetHandle)phid);
-    CPhidget_delete((CPhidgetHandle)phid);
+  ROS_INFO("Closing...");
+  CPhidget_close((CPhidgetHandle) phid);
+  CPhidget_delete((CPhidgetHandle) phid);
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "phidgets_high_speed_encoder");
-    ros::NodeHandle n;
-    ros::NodeHandle nh("~");
-    int serial_number = -1;
-    nh.getParam("serial", serial_number);
-    std::string name = "encoder";
-    nh.getParam("name", name);
-    if (serial_number==-1) {
-        nh.getParam("serial_number", serial_number);
-    }    ROS_INFO("%s, %d", frame_id.c_str(), (inverted ? 1ff : 0) );
+  ros::init(argc, argv, "phidgets_high_speed_encoder");
+  ros::NodeHandle n;
+  ros::NodeHandle nh("~");
+  int serial_number = -1;
+  nh.getParam("serial", serial_number);
+  std::string name = "encoder";
+  nh.getParam("name", name);
+  if (serial_number == -1)
+  {
+    nh.getParam("serial_number", serial_number);
+  }
+  ROS_INFO("frame_id = %s", frame_id.c_str());
+  if (inverted)
+  {
+    ROS_INFO("values are inverted");
+  }
 
-    std::string topic_path = "phidgets/";
-    nh.getParam("topic_path", topic_path);
-    nh.getParam("frame_id", frame_id);
-    nh.getParam("inverted", inverted);
+  std::string topic_path = "phidgets/";
+  nh.getParam("topic_path", topic_path);
+  nh.getParam("frame_id", frame_id);
+  nh.getParam("inverted", inverted);
 
-    if (attach(phid, serial_number)) {
-		display_properties(phid);
+  if (attach(phid, serial_number))
+  {
+    display_properties(phid);
 
-        const int buffer_length = 100;        
-        std::string topic_name = topic_path + name;
-        if (serial_number > -1) {
-            char ser[10];            
-            sprintf(ser,"%d", serial_number);
-            topic_name += "/";
-            topic_name += ser;
-        }
-        encoder_pub =
-			n.advertise<phidgets_high_speed_encoder::encoder_params>(topic_name,
-												  buffer_length);
-
-        initialised = true;
-
-        ros::spin();
-
-        disconnect(phid);
+    const int buffer_length = 100;
+    std::string topic_name = topic_path + name;
+    if (serial_number > -1)
+    {
+      char ser[10];
+      sprintf(ser, "%d", serial_number);
+      topic_name += "/";
+      topic_name += ser;
     }
-    return 0;
+    encoder_pub =
+        n.advertise<phidgets_high_speed_encoder::encoder_params>(topic_name,
+                                                                 buffer_length);
+
+    initialised = true;
+
+    ros::spin();
+
+    disconnect(phid);
+  }
+  return 0;
 }
 
