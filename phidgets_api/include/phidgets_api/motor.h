a36fb7f0daf27219a83387c43b134cebb7dca3b9
Zach Anderson <zach.inbox@gmail.com>
2017-02-20 20:12:39 -0600

    Added basic motor api

---
+++ phidgets_api/include/phidgets_api/motor.h
@@ -0,0 +1,68 @@
+#ifndef PHIDGETS_API_MOTOR_H
+#define PHIDGETS_API_MOTOR_H
+
+#include "phidgets_api/phidget.h"
+
+namespace phidgets
+{
+
+class MotorController: public Phidget
+{
+public:
+  MotorController();
+
+  // Motor specific
+  int    getMotorCount();
+  double getVelocity(int index);
+  void   setVelocity(int index, double velocity);
+  double getAcceleration(int index);
+  void   setAcceleration(int index, double acceleration);
+  double getAccelerationMax(int index);
+  double getAccelerationMin(int index);
+  double getCurrent(int index);
+
+  // Digital inputs
+  int  getInputCount();
+  bool getInputState(int index);
+
+  // Encoder inputs
+  int  getEncoderCount();
+  int  getEncoderPosition(int index);
+  void setEncoderPosition(int index, int position);
+
+  // Back EMF
+  int    getBackEMFSensingState(int index);
+  void   setBackEMFSensingState(int index, int bEMFState);
+  double getBackEMF(int index);
+
+  double getSupplyVoltage();
+
+  double getBraking(int index);
+  void   setBraking(int index, double braking);
+
+  // Analog sensors
+  int getSensorCount();
+  int getSensorValue(int index);
+  int getSensorRawValue(int index);
+
+  int  getRatiometric();
+  void setRatiometric(int ratiometric);
+
+  /* TODO Add event handlers from phidget api
+   * int CPhidgetMotorControl_set_OnVelocityChange_Handler (CPhidgetMotorControlHandle phid, int(*fptr)(CPhidgetMotorControlHandle phid, void *userPtr, int index, double velocity), void *userPtr)
+   * int CPhidgetMotorControl_set_OnCurrentChange_Handler (CPhidgetMotorControlHandle phid, int(*fptr)(CPhidgetMotorControlHandle phid, void *userPtr, int index, double current), void *userPtr)
+   * int CPhidgetMotorControl_set_OnInputChange_Handler (CPhidgetMotorControlHandle phid, int(*fptr)(CPhidgetMotorControlHandle phid, void *userPtr, int index, int inputState), void *userPtr)
+   * int CPhidgetMotorControl_set_OnEncoderPositionChange_Handler (CPhidgetMotorControlHandle phid, int(*fptr)(CPhidgetMotorControlHandle phid, void *userPtr, int index, int time, int positionChange), void *userPtr)
+   * int CPhidgetMotorControl_set_OnEncoderPositionUpdate_Handler (CPhidgetMotorControlHandle phid, int(*fptr)(CPhidgetMotorControlHandle phid, void *userPtr, int index, int positionChange), void *userPtr)
+   * int CPhidgetMotorControl_set_OnBackEMFUpdate_Handler (CPhidgetMotorControlHandle phid, int(*fptr)(CPhidgetMotorControlHandle phid, void *userPtr, int index, double voltage), void *userPtr)
+   * int CPhidgetMotorControl_set_OnSensorUpdate_Handler (CPhidgetMotorControlHandle phid, int(*fptr)(CPhidgetMotorControlHandle phid, void *userPtr, int index, int sensorValue), void *userPtr)
+   * int CPhidgetMotorControl_set_OnCurrentUpdate_Handler (CPhidgetMotorControlHandle phid, int(*fptr)(CPhidgetMotorControlHandle phid, void *userPtr, int index, double current), void *userPtr)
+   */
+
+protected:
+  CPhidgetMotorControlHandle motor_handle_;
+};
+
+} //namespace phidgets
+
+#endif // PHIDGETS_API_MOTOR_H
---
+++ phidgets_api/src/motor.cpp
@@ -0,0 +1,157 @@
+#include "phidgets_api/motor.h"
+
+#include <cassert>
+
+// Get and return the value name of type type (I varient is indexed)
+#define GAR(type, name)      type x; int ret = CPhidgetMotorControl_get ## name (motor_handle_, &x); assert(ret == EPHIDGET_OK); return x;
+#define GAR_I(type, name, i) type x; int ret = CPhidgetMotorControl_get ## name (motor_handle_, i, &x); assert(ret == EPHIDGET_OK); return x;
+
+// Set the value name to x (I varient is indexed)
+#define SET(name, x)      int ret = CPhidgetMotorControl_set ## name (motor_handle_, x); assert(ret == EPHIDGET_OK);
+#define SET_I(name, x, i) int ret = CPhidgetMotorControl_set ## name (motor_handle_, i, x); assert(ret == EPHIDGET_OK);
+
+namespace phidgets
+{
+
+MotorController::MotorController():
+  Phidget(),
+  motor_handle_(0)
+{
+  // create the handle
+  CPhidgetMotorControl_create(&motor_handle_);
+
+  // pass handle to base class
+  Phidget::init((CPhidgetHandle)motor_handle_);
+
+  // register base class callbacks
+  Phidget::registerHandlers();
+}
+
+int MotorController::getMotorCount()
+{
+  GAR(int, MotorCount);
+}
+
+double MotorController::getVelocity(int index)
+{
+  GAR_I(double, Velocity, index);
+}
+
+void MotorController::setVelocity(int index, double velocity)
+{
+  SET_I(Velocity, velocity, index);
+}
+
+double MotorController::getAcceleration(int index)
+{
+  GAR_I(double, Acceleration, index);
+}
+
+void MotorController::setAcceleration(int index, double acceleration)
+{
+  SET_I(Acceleration, acceleration, index);
+}
+
+double MotorController::getAccelerationMax(int index)
+{
+  GAR_I(double, AccelerationMax, index);
+}
+
+double MotorController::getAccelerationMin(int index)
+{
+  GAR_I(double, AccelerationMin, index);
+}
+
+double MotorController::getCurrent(int index)
+{
+  GAR_I(double, Current, index);
+}
+
+int MotorController::getInputCount()
+{
+  GAR(int, InputCount);
+}
+
+bool MotorController::getInputState(int index)
+{
+  int state;
+  int ret = CPhidgetMotorControl_getInputState(motor_handle_, index, &state);
+
+  assert(ret == EPHIDGET_OK);
+
+  return state == PTRUE;
+}
+
+int MotorController::getEncoderCount()
+{
+  GAR(int, EncoderCount);
+}
+
+int MotorController::getEncoderPosition(int index)
+{
+  GAR_I(int, EncoderPosition, index);
+}
+
+void MotorController::setEncoderPosition(int index, int position)
+{
+  SET_I(EncoderPosition, position, index);
+}
+
+int MotorController::getBackEMFSensingState(int index)
+{
+  GAR_I(int, BackEMFSensingState, index);
+}
+
+void MotorController::setBackEMFSensingState(int index, int bEMFState)
+{
+  SET_I(BackEMFSensingState, bEMFState, index);
+}
+
+double MotorController::getBackEMF(int index)
+{
+  GAR_I(double, BackEMF, index);
+}
+
+double MotorController::getSupplyVoltage()
+{
+  GAR(double, SupplyVoltage);
+}
+
+double MotorController::getBraking(int index)
+{
+  GAR_I(double, Braking, index);
+}
+
+void MotorController::setBraking(int index, double braking)
+{
+  SET_I(Braking, braking, index);
+}
+
+int MotorController::getSensorCount()
+{
+  GAR(int, SensorCount);
+}
+
+int MotorController::getSensorValue(int index)
+{
+  GAR_I(int, SensorValue, index);
+}
+
+int MotorController::getSensorRawValue(int index)
+{
+  GAR_I(int, SensorRawValue, index);
+}
+
+int MotorController::getRatiometric()
+{
+  GAR(int, Ratiometric);
+}
+
+void MotorController::setRatiometric(int ratiometric)
+{
+  SET(Ratiometric, ratiometric);
+}
+
+
+} //namespace phidgets
+
