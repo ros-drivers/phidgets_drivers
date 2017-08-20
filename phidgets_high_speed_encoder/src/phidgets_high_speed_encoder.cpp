8297bedc360cb37406fd6e0bcae48a193f2bdf0c
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2017-08-20 16:45:46 +0200

    Rewrite encoder node to use phidgets::Encoder

--- phidgets_high_speed_encoder/src/phidgets_high_speed_encoder.cpp
+++ phidgets_high_speed_encoder/src/phidgets_high_speed_encoder.cpp
@@ -35,15 +35,15 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <sstream>
-#include <libphidget21/phidget21.h>
 #include <std_msgs/String.h>
 #include <vector>
 #include <mutex>
 #include <numeric> // std::accumulate()
+#include "phidgets_api/encoder.h"
+
 #include "phidgets_high_speed_encoder/EncoderParams.h"
 #include "phidgets_high_speed_encoder/EncoderDecimatedSpeed.h"
 
-static CPhidgetEncoderHandle phid;
 std::vector<ros::Publisher> encoder_pubs, encoder_decimspeed_pubs;
 static std::string frame_id;
 static bool inverted = false;
@@ -65,306 +65,243 @@ struct TStatePerChannel
 std::vector<TStatePerChannel>  encoder_states;
 std::mutex  encoder_states_mux;
 
-
-int AttachHandler(CPhidgetHandle phid, void *userptr)
+/** Derived type from Encoder so we can override virtual event handlers. */
+class EncoderNode : public phidgets::Encoder
 {
-  int serial_number;
-  const char *name;
+public:
+	EncoderNode() : phidgets::Encoder()
+	{
+	}
 
-  CPhidget_getDeviceName(phid, &name);
-  CPhidget_getSerialNumber(phid, &serial_number);
+	void display_properties()
+	{
+		const int serial_number = Phidget::getDeviceSerialNumber();
+		const int version = Phidget::getDeviceVersion();
+		const int num_encoders = Encoder::getEncoderCount();
+		const std::string dev_type = Phidget::getDeviceType();
+		const int num_inputs = Encoder::getInputCount();
 
-  CPhidget_DeviceID deviceID;
-  //Retrieve the device ID and number of encoders so that we can set the enables if needed
-  CPhidget_getDeviceID(phid, &deviceID);
-  int inputcount=0;
-  CPhidgetEncoder_getEncoderCount((CPhidgetEncoderHandle)phid, &inputcount);
+		ROS_INFO("Device type       : %s", dev_type.c_str());
+		ROS_INFO("Device serial     : %d", serial_number);
+		ROS_INFO("Device version    : %d", version);
+		ROS_INFO("Number of encoders: %d", num_encoders);
+		ROS_INFO("Number of inputs  : %d", num_inputs);
+	}
 
-  {
-    std::lock_guard<std::mutex> lock(encoder_states_mux);
-    encoder_states.resize(inputcount);
-  }
-  ROS_INFO("%s Serial number %d attached with %d encoders!",
-    name, serial_number,inputcount);
+protected:
+	void attachHandler() override
+	{
+		const int serial_number = Phidget::getDeviceSerialNumber();
+		const std::string name = Phidget::getDeviceName();
+		const int inputcount = Encoder::getEncoderCount();
 
-  //the 1047 requires enabling of the encoder inputs, so enable them if this is a 1047
-  if (deviceID == PHIDID_ENCODER_HS_4ENCODER_4INPUT)
-  {
-      for (int i = 0 ; i < inputcount ; i++)
-        CPhidgetEncoder_setEnabled((CPhidgetEncoderHandle)phid, i, 1);
-  }
+		{
+			std::lock_guard<std::mutex> lock(encoder_states_mux);
+			encoder_states.resize(inputcount);
+		}
+		ROS_INFO("%s Serial number %d attached with %d encoders!",
+				 name.c_str(), serial_number,inputcount);
 
-  return 0;
-}
+		//the 1047 requires enabling of the encoder inputs, so enable them if this is a 1047
+		for (int i = 0 ; i < inputcount ; i++)
+			Encoder::setEnabled(i, true);
+	}
 
-int DetachHandler(CPhidgetHandle phid, void *userptr)
-{
-  int serial_number;
-  const char *name;
+	void detachHandler() override
+	{
+		const int serial_number = Phidget::getDeviceSerialNumber();
+		const std::string name = Phidget::getDeviceName();
+		ROS_INFO("%s Serial number %d detached!",
+				 name.c_str(), serial_number);
+	}
 
-  CPhidget_getDeviceName(phid, &name);
-  CPhidget_getSerialNumber(phid, &serial_number);
-  ROS_INFO("%s Serial number %d detached!",
-           name, serial_number);
+	void errorHandler(int ErrorCode) override
+	{
+		ROS_ERROR("Error handled. %d - %s", ErrorCode,
+				  Phidget::getErrorDescription(ErrorCode).c_str());
+	}
 
-  return 0;
-}
+	void positionChangeHandler(int index, int time, int positionChange) override
+	{
+		const int Position = Encoder::getPosition(index);
+		ROS_DEBUG("Encoder %d Count %d", index, Position);
 
-int ErrorHandler(CPhidgetHandle phid, void *userptr,
-                 int ErrorCode, const char *Description)
-{
-  ROS_INFO("Error handled. %d - %s", ErrorCode, Description);
-  return 0;
-}
+		std::lock_guard<std::mutex> lock(encoder_states_mux);
+		if (index<(int)encoder_states.size())
+		{
+		  TStatePerChannel &spc = encoder_states[index];
+		  spc.tickPos = Position;
+		  spc.instantaneousSpeed = positionChange / (time*1e-6);
+		  spc.speeds_buffer.push_back(spc.instantaneousSpeed);
+		  spc.speed_buffer_updated = true;
+		  spc.loops_without_update_speed_buffer = 0;
+		}
+	}
 
-int InputChangeHandler(CPhidgetEncoderHandle ENC,
-                       void *usrptr, int Index, int State)
-{
-  return 0;
-}
+	void indexHandler(int index, int indexPosition) override
+	{
+
+	}
+
+
+}; // end EncoderNode
 
 int PositionChangeHandler(CPhidgetEncoderHandle ENC,
                           void *usrptr, int Index,
                           int Time, int RelativePosition)
 {
-  int Position;
-  CPhidgetEncoder_getPosition(ENC, Index, &Position);
-  ROS_DEBUG("Encoder %d Count %d", Index, Position);
-
-  std::lock_guard<std::mutex> lock(encoder_states_mux);
-  if (Index<encoder_states.size())
-  {
-    TStatePerChannel &spc = encoder_states[Index];
-    spc.tickPos = Position;
-    spc.instantaneousSpeed = RelativePosition / (Time*1e-6);
-    spc.speeds_buffer.push_back(spc.instantaneousSpeed);
-    spc.speed_buffer_updated = true;
-    spc.loops_without_update_speed_buffer = 0;
-  }
 
   return 0;
-}
-
-int display_properties(CPhidgetEncoderHandle phid)
-{
-  int serial_number, version, num_encoders, num_inputs;
-  const char *ptr;
-
-  CPhidget_getDeviceType((CPhidgetHandle) phid, &ptr);
-  CPhidget_getSerialNumber((CPhidgetHandle) phid,
-                           &serial_number);
-  CPhidget_getDeviceVersion((CPhidgetHandle) phid, &version);
-
-  CPhidgetEncoder_getInputCount(phid, &num_inputs);
-  CPhidgetEncoder_getEncoderCount(phid, &num_encoders);
-
-  ROS_INFO("%s", ptr);
-  ROS_INFO("Serial Number: %d", serial_number);
-  ROS_INFO("Version: %d", version);
-  ROS_INFO("Number of encoders %d", num_encoders);
-
-  return 0;
-}
-
-bool attach(
-    CPhidgetEncoderHandle &phid,
-    int serial_number)
-{
-  // create the object
-  CPhidgetEncoder_create(&phid);
-
-  // Set the handlers to be run when the device is
-  // plugged in or opened from software, unplugged or
-  // closed from software, or generates an error.
-  CPhidget_set_OnAttach_Handler((CPhidgetHandle) phid,
-                                AttachHandler, NULL);
-  CPhidget_set_OnDetach_Handler((CPhidgetHandle) phid,
-                                DetachHandler, NULL);
-  CPhidget_set_OnError_Handler((CPhidgetHandle) phid,
-                               ErrorHandler, NULL);
-
-  // Registers a callback that will run if an input changes.
-  // Requires the handle for the Phidget, the function
-  // that will be called, and an arbitrary pointer that
-  // will be supplied to the callback function (may be NULL).
-  CPhidgetEncoder_set_OnInputChange_Handler(phid,
-                                            InputChangeHandler,
-                                            NULL);
-
-  // Registers a callback that will run if the
-  // encoder changes.
-  // Requires the handle for the Encoder, the function
-  // that will be called, and an arbitrary pointer that
-  // will be supplied to the callback function (may be NULL).
-  CPhidgetEncoder_set_OnPositionChange_Handler(phid,
-                                               PositionChangeHandler,
-                                               NULL);
-
-  //open the device for connections
-  CPhidget_open((CPhidgetHandle) phid, serial_number);
-
-  // get the program to wait for an encoder device
-  // to be attached
-  if (serial_number == -1)
-  {
-    ROS_INFO("Waiting for High Speed Encoder Phidget " \
-         "to be attached....");
-  }
-  else
-  {
-    ROS_INFO("Waiting for High Speed Encoder Phidget " \
-         "%d to be attached....", serial_number);
-  }
-  int result;
-  if ((result =
-           CPhidget_waitForAttachment((CPhidgetHandle) phid,
-                                      10000)))
-  {
-    const char *err;
-    CPhidget_getErrorDescription(result, &err);
-    ROS_ERROR("Problem waiting for attachment: %s",
-              err);
-    return false;
-  }
-  else return true;
-}
-
-/*!
- * \brief disconnect the encoder
- */
-void disconnect(CPhidgetEncoderHandle &phid)
-{
-  ROS_INFO("Closing...");
-  CPhidget_close((CPhidgetHandle) phid);
-  CPhidget_delete((CPhidgetHandle) phid);
 }
 
 int main(int argc, char *argv[])
 {
-  ros::init(argc, argv, "phidgets_high_speed_encoder");
-  ros::NodeHandle n;
-  ros::NodeHandle nh("~");
-  int serial_number = -1;
-  nh.getParam("serial", serial_number);
-  std::string name = "encoder";
-  nh.getParam("name", name);
-  if (serial_number == -1)
-  {
-    nh.getParam("serial_number", serial_number);
-  }
-  if (inverted)
-  {
-    ROS_INFO("values are inverted");
-  }
+	ros::init(argc, argv, "phidgets_high_speed_encoder");
+	ros::NodeHandle n;
+	ros::NodeHandle nh("~");
+	int serial_number = -1;
+	nh.getParam("serial", serial_number);
+	std::string name = "encoder";
+	nh.getParam("name", name);
+	if (serial_number == -1)
+	{
+	nh.getParam("serial_number", serial_number);
+	}
+	if (inverted)
+	{
+	ROS_INFO("values are inverted");
+	}
 
-  std::string topic_path = "phidgets/";
-  nh.getParam("topic_path", topic_path);
-  nh.getParam("frame_id", frame_id);
-  ROS_INFO("frame_id = %s", frame_id.c_str());
-  nh.getParam("inverted", inverted);
-  nh.getParam("PUBLISH_RATE", PUBLISH_RATE);
-  nh.getParam("SPEED_FILTER_SAMPLES_LEN", SPEED_FILTER_SAMPLES_LEN);
-  nh.getParam("SPEED_FILTER_IDLE_ITER_LOOPS_BEFORE_RESET", SPEED_FILTER_IDLE_ITER_LOOPS_BEFORE_RESET);
+	std::string topic_path = "phidgets/";
+	nh.getParam("topic_path", topic_path);
+	nh.getParam("frame_id", frame_id);
+	ROS_INFO("frame_id = %s", frame_id.c_str());
+	nh.getParam("inverted", inverted);
+	nh.getParam("PUBLISH_RATE", PUBLISH_RATE);
+	nh.getParam("SPEED_FILTER_SAMPLES_LEN", SPEED_FILTER_SAMPLES_LEN);
+	nh.getParam("SPEED_FILTER_IDLE_ITER_LOOPS_BEFORE_RESET", SPEED_FILTER_IDLE_ITER_LOOPS_BEFORE_RESET);
 
 	std::string topic_name = topic_path + name;
 	if (serial_number > -1)
 	{
-		char ser[10];
+		char ser[100];
 		sprintf(ser, "%d", serial_number);
 		topic_name += "/";
 		topic_name += ser;
 	}
 
-	if (attach(phid, serial_number))
+	// The encoder object instance:
+	EncoderNode  encoder_node;
+
+	//open the device:
+	encoder_node.open(serial_number);
+
+	// get the program to wait for an encoder device to be attached
+	if (serial_number == -1)
 	{
-		display_properties(phid);
+		ROS_INFO("Waiting for any High Speed Encoder Phidget to be attached.");
+	}
+	else
+	{
+		ROS_INFO("Waiting for High Speed Encoder Phidget %d to be attached....",
+				 serial_number);
+	}
 
-		ros::Rate rate(PUBLISH_RATE);
-		ROS_INFO("Publishing encoder states at %.03f Hz",PUBLISH_RATE);
-		while (ros::ok())
+	const int result = encoder_node.waitForAttachment(10000);
+
+	if (result!=0)
+	{
+		const auto sError = encoder_node.getErrorDescription(result);
+		ROS_ERROR("Problem waiting for attachment: %s",sError.c_str());
+		return 1; // !=0 exit status means error in main()
+	}
+
+	encoder_node.display_properties();
+
+	ros::Rate rate(PUBLISH_RATE);
+	ROS_INFO("Publishing encoder states at %.03f Hz",PUBLISH_RATE);
+	while (ros::ok())
+	{
+		ros::spinOnce();
+		rate.sleep();
+
+		// Publish:
 		{
-			ros::spinOnce();
-			rate.sleep();
+			std::lock_guard<std::mutex> lock(encoder_states_mux);
+			const unsigned int N = encoder_states.size();
 
-			// Publish:
+			// First time: create publishers:
+			ROS_ASSERT(encoder_decimspeed_pubs.size()==encoder_pubs.size());
+			if (encoder_pubs.size()!=N)
 			{
-				std::lock_guard<std::mutex> lock(encoder_states_mux);
-				const unsigned int N = encoder_states.size();
-
-				// First time: create publishers:
-				ROS_ASSERT(encoder_decimspeed_pubs.size()==encoder_pubs.size());
-				if (encoder_pubs.size()!=N)
-				{
-					encoder_pubs.resize(N);
-					encoder_decimspeed_pubs.resize(N);
-					for (unsigned int i=0;i<N;i++)
-					{
-						std::string pub_name = topic_name;
-						char ch[10];
-						sprintf(ch, "/channel%u", i);
-						pub_name += ch;
-						ROS_INFO("Publishing state to topic: %s",pub_name.c_str());
-						encoder_pubs[i] =
-							n.advertise<phidgets_high_speed_encoder::EncoderParams>(
-									pub_name,
-									100);
-
-						pub_name+="_decim_speed";
-						ROS_INFO("Publishing decimated speed to topic: %s",pub_name.c_str());
-						encoder_decimspeed_pubs[i] =
-							n.advertise<phidgets_high_speed_encoder::EncoderDecimatedSpeed>(
-									pub_name,
-									10);
-					}
-				}
-
+				encoder_pubs.resize(N);
+				encoder_decimspeed_pubs.resize(N);
 				for (unsigned int i=0;i<N;i++)
 				{
-					TStatePerChannel &spc = encoder_states[i];
+					std::string pub_name = topic_name;
+					char ch[10];
+					sprintf(ch, "/channel%u", i);
+					pub_name += ch;
+					ROS_INFO("Publishing state to topic: %s",pub_name.c_str());
+					encoder_pubs[i] =
+						n.advertise<phidgets_high_speed_encoder::EncoderParams>(
+								pub_name,
+								100);
 
-					phidgets_high_speed_encoder::EncoderParams e;
-					e.header.stamp = ros::Time::now();
-					e.header.frame_id = frame_id;
-					e.count = (inverted ? -spc.tickPos : spc.tickPos);
-					e.inst_vel = spc.instantaneousSpeed;
-					spc.instantaneousSpeed = 0; // Reset speed
-					encoder_pubs[i].publish(e);
+					pub_name+="_decim_speed";
+					ROS_INFO("Publishing decimated speed to topic: %s",pub_name.c_str());
+					encoder_decimspeed_pubs[i] =
+						n.advertise<phidgets_high_speed_encoder::EncoderDecimatedSpeed>(
+								pub_name,
+								10);
+				}
+			}
 
-					if (SPEED_FILTER_SAMPLES_LEN>0)
+			for (unsigned int i=0;i<N;i++)
+			{
+				TStatePerChannel &spc = encoder_states[i];
+
+				phidgets_high_speed_encoder::EncoderParams e;
+				e.header.stamp = ros::Time::now();
+				e.header.frame_id = frame_id;
+				e.count = (inverted ? -spc.tickPos : spc.tickPos);
+				e.inst_vel = spc.instantaneousSpeed;
+				spc.instantaneousSpeed = 0; // Reset speed
+				encoder_pubs[i].publish(e);
+
+				if (SPEED_FILTER_SAMPLES_LEN>0)
+				{
+					if (!spc.speed_buffer_updated)
 					{
-						if (!spc.speed_buffer_updated)
+						if (++spc.loops_without_update_speed_buffer >= SPEED_FILTER_IDLE_ITER_LOOPS_BEFORE_RESET)
 						{
-							if (++spc.loops_without_update_speed_buffer >= SPEED_FILTER_IDLE_ITER_LOOPS_BEFORE_RESET)
-							{
-								phidgets_high_speed_encoder::EncoderDecimatedSpeed e;
-								e.header.stamp = ros::Time::now();
-								e.header.frame_id = frame_id;
-								e.speed = .0;
-								encoder_decimspeed_pubs[i].publish(e);
-							}
+							phidgets_high_speed_encoder::EncoderDecimatedSpeed e;
+							e.header.stamp = ros::Time::now();
+							e.header.frame_id = frame_id;
+							e.speed = .0;
+							encoder_decimspeed_pubs[i].publish(e);
 						}
-						else
+					}
+					else
+					{
+						spc.loops_without_update_speed_buffer = 0;
+
+						if (spc.speeds_buffer.size()>=SPEED_FILTER_SAMPLES_LEN)
 						{
-							spc.loops_without_update_speed_buffer = 0;
+							const double avrg = std::accumulate(spc.speeds_buffer.begin(),spc.speeds_buffer.end(),0.0) / spc.speeds_buffer.size();
+							spc.speeds_buffer.clear();
 
-							if (spc.speeds_buffer.size()>=SPEED_FILTER_SAMPLES_LEN)
-							{
-								const double avrg = std::accumulate(spc.speeds_buffer.begin(),spc.speeds_buffer.end(),0.0) / spc.speeds_buffer.size();
-								spc.speeds_buffer.clear();
-
-								phidgets_high_speed_encoder::EncoderDecimatedSpeed e;
-								e.header.stamp = ros::Time::now();
-								e.header.frame_id = frame_id;
-								e.speed = avrg;
-								encoder_decimspeed_pubs[i].publish(e);
-							}
+							phidgets_high_speed_encoder::EncoderDecimatedSpeed e;
+							e.header.stamp = ros::Time::now();
+							e.header.frame_id = frame_id;
+							e.speed = avrg;
+							encoder_decimspeed_pubs[i].publish(e);
 						}
 					}
 				}
-			} // end lock guard
-		} // end while ros::ok()
-	} // end if attached()
+			}
+		} // end lock guard
+	} // end while ros::ok()
 
-	disconnect(phid);
-  return 0;
+	return 0;
 }
