310c649f884075f8967818e306cd0e4f38a8f38f
russel <russel@appliedinvention.com>
2017-11-15 09:39:00 -0800

    Squashed commit of the following
    These commits are copied from
    https://github.com/jsarrett/usma_phidgets
    An attempt was made to up-stream the changes from there, but he never got a
    response. I squashed all the commits and applied the resulting patch to this
    repo.
    
    commit 581479e258da50948ba137e84ac9f74beeb8d809
    Author: James Sarrett <jsarrett@gmail.com>
    Date:   Fri Feb 10 13:05:36 2017 -0800
    
    add paramter to open a specifc phidget serial number to PhidgetIK
    
    commit 8f1c76c422840cb190f2fa440717ba12b4633c5c
    Author: James Sarrett <jsarrett@gmail.com>
    Date:   Fri Feb 10 10:27:18 2017 -0800
    
    create topics to set output states
    
    commit cf27bab6a15ec3bc161b215695fc500f6395bd03
    Author: James Sarrett <jsarrett@gmail.com>
    Date:   Fri Feb 10 09:34:36 2017 -0800
    
    use std::vector instead of fixed size array for inputs and sensors
    
    commit 2b8fa829d9b12d9a2da837c77d4608b514c529fe
    Author: James Sarrett <jsarrett@gmail.com>
    Date:   Fri Feb 10 03:46:19 2017 -0800
    
    don't publish unless the publisher is setup
    
    commit fd92d7a2c83649f641c3925790efe1db3cc7ec61
    Author: James Sarrett <jsarrett@gmail.com>
    Date:   Fri Feb 10 03:21:34 2017 -0800
    
    BROKEN, but the right idea.  need ot keep reference ot publisher instead of copy.
    
    commit bdf13af2703e8f5ce533a401d22ff441c33fcbc0
    Author: James Sarrett <jsarrett@gmail.com>
    Date:   Fri Feb 10 00:04:47 2017 -0800
    
    copy phidgets_ir note and modify it to open an InterfaceKit

---
+++ phidgets_ik/src/ik_ros_i.cpp
@@ -0,0 +1,98 @@
+#include "phidgets_ik/ik_ros_i.h"
+#include <stdio.h>
+
+namespace phidgets {
+
+IKRosI::IKRosI(ros::NodeHandle nh, ros::NodeHandle nh_private):
+  IK(),
+  n_in(0),
+  n_out(0),
+  n_sensors(0),
+  nh_(nh),
+  nh_private_(nh_private)
+{
+  ROS_INFO ("Starting Phidgets IK");
+
+  initDevice();
+}
+
+void IKRosI::initDevice()
+{
+  ROS_INFO("Opening device");
+  int serial_num;
+  if (!nh_private_.getParam("serial", serial_num))
+    serial_num = -1; //defalut open any device
+  open(serial_num);
+
+  ROS_INFO("Waiting for IK to be attached...");
+  int result = waitForAttachment(10000);
+  if(result)
+  {
+    const char *err;
+    CPhidget_getErrorDescription(result, &err);
+    ROS_FATAL("Problem waiting for IK attachment: %s", err);
+    exit(result);
+  }
+
+  CPhidgetInterfaceKit_getInputCount(ik_handle_, &n_in);
+  CPhidgetInterfaceKit_getOutputCount(ik_handle_, &n_out);
+  CPhidgetInterfaceKit_getSensorCount(ik_handle_, &n_sensors);
+  ROS_INFO("%d inputs, %d outputs, %d sensors", n_in, n_out, n_sensors);
+  for (int i = 0; i < n_in; i++) {
+    char topicname[] = "input00";
+    snprintf(topicname, sizeof(topicname), "input%02d", i);
+    in_pubs_.push_back(nh_.advertise<std_msgs::Bool>(topicname, 1));
+  }
+  for (int i = 0; i < n_out; i++) {
+    char topicname[] = "output00";
+    snprintf(topicname, sizeof(topicname), "output%02d", i);
+    output_setter * s = new output_setter(ik_handle_, i);
+    s->subscription = nh_.subscribe(topicname, 1, &output_setter::set_msg_callback, s);
+    out_subs_.push_back(s);
+  }
+  for (int i = 0; i < n_sensors; i++) {
+    char topicname[] = "sensor00";
+    snprintf(topicname, sizeof(topicname), "sensor%02d", i);
+    sensor_pubs_.push_back(nh_.advertise<std_msgs::Float32>(topicname, 1));
+  }
+}
+
+void IKRosI::sensorHandler(int index, int sensorValue)
+{
+  // do nothing - just refer to base class callbalck, which prints the values
+  IK::sensorHandler(index, sensorValue);
+  //get rawsensorvalue and divide by 4096, which according to the documentation for both the IK888 and IK222 are the maximum senosr value
+  int rawval = 0;
+  CPhidgetInterfaceKit_getSensorRawValue(ik_handle_, index, &rawval);
+  std_msgs::Float32 msg;
+  msg.data = float(rawval)/4095.0f;
+  if ((sensor_pubs_.size() > index) && (sensor_pubs_[index])) {
+    sensor_pubs_[index].publish(msg);
+  }
+}
+
+void IKRosI::inputHandler(int index, int inputValue)
+{
+  // do nothing - just refer to base class callbalck, which prints the values
+  IK::inputHandler(index, inputValue);
+  std_msgs::Bool msg;
+  msg.data = inputValue != 0;
+  if ((in_pubs_.size() > index) && (in_pubs_[index])) {
+    in_pubs_[index].publish(msg);
+  }
+}
+
+void output_setter::set_msg_callback(const std_msgs::Bool::ConstPtr& msg)
+{
+  ROS_INFO("Setting output %d to %d", index, msg->data);
+  CPhidgetInterfaceKit_setOutputState(ik_handle_, index, msg->data);
+}
+
+output_setter::output_setter(CPhidgetInterfaceKitHandle ik_handle, int index)
+{
+    this->ik_handle_ = ik_handle_;
+    this->index = index;
+}
+
+} // namespace phidgets
+
