240850f2926006a257406f0090bd225b0b226957
Chris Lalancette <clalancette@openrobotics.org>
2019-09-06 20:49:29 +0000

    Port gyroscope to ROS 2.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- phidgets_gyroscope/include/phidgets_gyroscope/gyroscope_ros_i.hpp
+++ phidgets_gyroscope/include/phidgets_gyroscope/gyroscope_ros_i.hpp
@@ -27,26 +27,66 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PHIDGETS_GYROSCOPE_PHIDGETS_GYROSCOPE_NODELET_H
-#define PHIDGETS_GYROSCOPE_PHIDGETS_GYROSCOPE_NODELET_H
+#ifndef PHIDGETS_GYROSCOPE_GYROSCOPE_ROS_I_H
+#define PHIDGETS_GYROSCOPE_GYROSCOPE_ROS_I_H
 
 #include <memory>
+#include <mutex>
+#include <string>
 
-#include <nodelet/nodelet.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_srvs/srv/empty.hpp>
 
-#include "phidgets_gyroscope/gyroscope_ros_i.h"
+#include "phidgets_api/gyroscope.hpp"
 
 namespace phidgets {
 
-class PhidgetsGyroscopeNodelet : public nodelet::Nodelet
+class GyroscopeRosI final : public rclcpp::Node
 {
   public:
-    virtual void onInit();
+    explicit GyroscopeRosI(const rclcpp::NodeOptions& options);
 
   private:
-    std::unique_ptr<GyroscopeRosI> gyroscope_;
+    std::unique_ptr<Gyroscope> gyroscope_;
+    std::string frame_id_;
+    double angular_velocity_variance_;
+    std::mutex gyro_mutex_;
+    double last_gyro_x_;
+    double last_gyro_y_;
+    double last_gyro_z_;
+
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr cal_publisher_;
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr cal_srv_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr gyroscope_pub_;
+    void timerCallback();
+    rclcpp::TimerBase::SharedPtr timer_;
+    int publish_rate_;
+
+    rclcpp::Time ros_time_zero_;
+    bool synchronize_timestamps_{true};
+    uint64_t data_time_zero_ns_{0};
+    uint64_t last_data_timestamp_ns_{0};
+    uint64_t last_ros_stamp_ns_{0};
+    int64_t time_resync_interval_ns_{0};
+    int64_t data_interval_ns_{0};
+    bool can_publish_{false};
+    rclcpp::Time last_cb_time_;
+    int64_t cb_delta_epsilon_ns_{0};
+
+    void publishLatest();
+
+    void calibrate();
+
+    void calibrateService(
+        const std::shared_ptr<std_srvs::srv::Empty::Request> req,
+        std::shared_ptr<std_srvs::srv::Empty::Response> res);
+
+    void gyroscopeChangeCallback(const double angular_rate[3],
+                                 const double timestamp);
 };
 
 }  // namespace phidgets
 
-#endif  // PHIDGETS_GYROSCOPE_PHIDGETS_GYROSCOPE_NODELET_H
+#endif  // PHIDGETS_GYROSCOPE_GYROSCOPE_ROS_I_H
