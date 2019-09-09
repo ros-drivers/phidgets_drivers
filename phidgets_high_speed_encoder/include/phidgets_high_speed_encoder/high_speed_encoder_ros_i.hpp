ef1ba6d183e0221a9b79810c54ddab03ce2a070f
Chris Lalancette <clalancette@openrobotics.org>
2019-09-09 19:21:37 +0000

    Port high speed encoder to ROS 2.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- phidgets_high_speed_encoder/include/phidgets_high_speed_encoder/high_speed_encoder_ros_i.hpp
+++ phidgets_high_speed_encoder/include/phidgets_high_speed_encoder/high_speed_encoder_ros_i.hpp
@@ -27,26 +27,60 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PHIDGETS_HIGH_SPEED_ENCODER_PHIDGETS_HIGH_SPEED_ENCODER_NODELET_H
-#define PHIDGETS_HIGH_SPEED_ENCODER_PHIDGETS_HIGH_SPEED_ENCODER_NODELET_H
+#ifndef PHIDGETS_HIGH_SPEED_ENCODER_HIGH_SPEED_ENCODER_ROS_I_H
+#define PHIDGETS_HIGH_SPEED_ENCODER_HIGH_SPEED_ENCODER_ROS_I_H
 
 #include <memory>
+#include <mutex>
+#include <string>
+#include <vector>
 
-#include <nodelet/nodelet.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 
-#include "phidgets_high_speed_encoder/high_speed_encoder_ros_i.h"
+#include "phidgets_api/encoders.hpp"
+#include "phidgets_msgs/msg/encoder_decimated_speed.hpp"
 
 namespace phidgets {
 
-class PhidgetsHighSpeedEncoderNodelet : public nodelet::Nodelet
-{
-  public:
-    virtual void onInit();
-
-  private:
-    std::unique_ptr<HighSpeedEncoderRosI> enc_;
+struct EncoderDataToPub {
+    double instantaneous_speed = .0;
+    std::vector<double> speeds_buffer;
+    bool speed_buffer_updated = false;
+    int loops_without_update_speed_buffer = 0;
+    std::string joint_name;
+    double joint_tick2rad;
+    rclcpp::Publisher<phidgets_msgs::msg::EncoderDecimatedSpeed>::SharedPtr
+        encoder_decimspeed_pub;
 };
 
+class HighSpeedEncoderRosI final : public rclcpp::Node
+{
+  public:
+    explicit HighSpeedEncoderRosI(const rclcpp::NodeOptions& options);
+
+  private:
+    std::unique_ptr<Encoders> encs_;
+    std::mutex encoder_mutex_;
+    std::vector<EncoderDataToPub> enc_data_to_pub_;
+    std::string frame_id_;
+    // (Default=10) Number of samples for the sliding window average filter of
+    // speeds.
+    int speed_filter_samples_len_;
+    // (Default=1) Number of "ITERATE" loops without any new encoder tick before
+    // resetting the filtered average velocities.
+    int speed_filter_idle_iter_loops_before_reset_;
+
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr encoder_pub_;
+    void timerCallback();
+    rclcpp::TimerBase::SharedPtr timer_;
+    int publish_rate_;
+
+    void publishLatest(int channel);
+
+    void positionChangeHandler(int channel, int position_change, double time,
+                               int index_triggered);
+};
 }  // namespace phidgets
 
-#endif  // PHIDGETS_HIGH_SPEED_ENCODER_PHIDGETS_HIGH_SPEED_ENCODER_NODELET_H
+#endif  // PHIDGETS_HIGH_SPEED_ENCODER_HIGH_SPEED_ENCODER_ROS_I_H
