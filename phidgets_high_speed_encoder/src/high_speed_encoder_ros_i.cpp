4d871dd029165dacba1cc7766381b24f0342ebb0
Chris Lalancette <clalancette@openrobotics.org>
2019-09-09 19:21:37 +0000

    Port high speed encoder to ROS 2.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- phidgets_high_speed_encoder/src/high_speed_encoder_ros_i.cpp
+++ phidgets_high_speed_encoder/src/high_speed_encoder_ros_i.cpp
@@ -30,53 +30,49 @@
 #include <functional>
 #include <memory>
 #include <numeric>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-#include <sensor_msgs/JointState.h>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 
-#include "phidgets_high_speed_encoder/high_speed_encoder_ros_i.h"
-#include "phidgets_msgs/EncoderDecimatedSpeed.h"
+#include "phidgets_high_speed_encoder/high_speed_encoder_ros_i.hpp"
+#include "phidgets_msgs/msg/encoder_decimated_speed.hpp"
 
 namespace phidgets {
 
-HighSpeedEncoderRosI::HighSpeedEncoderRosI(ros::NodeHandle nh,
-                                           ros::NodeHandle nh_private)
-    : nh_(nh), nh_private_(nh_private)
+HighSpeedEncoderRosI::HighSpeedEncoderRosI(const rclcpp::NodeOptions& options)
+    : rclcpp::Node("phidgets_high_speed_encoder_node", options)
 {
-    ROS_INFO("Starting Phidgets Encoders");
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
-    int serial_num;
-    if (!nh_private_.getParam("serial", serial_num))
+    RCLCPP_INFO(get_logger(), "Starting Phidgets Encoders");
+
+    int serial_num =
+        this->declare_parameter("serial", -1);  // default open any device
+
+    int hub_port = this->declare_parameter(
+        "hub_port", 0);  // only used if the device is on a VINT hub_port
+
+    frame_id_ = this->declare_parameter("frame_id", "encoder_link");
+
+    speed_filter_samples_len_ =
+        this->declare_parameter("speed_filter_samples_len", 10);
+
+    speed_filter_idle_iter_loops_before_reset_ =
+        this->declare_parameter("speed_filter_idle_iter_loops_before_reset", 1);
+
+    publish_rate_ = this->declare_parameter("publish_rate", 0);
+    if (publish_rate_ > 1000)
     {
-        serial_num = -1;  // default open any device
-    }
-    int hub_port;
-    if (!nh_private.getParam("hub_port", hub_port))
-    {
-        hub_port = 0;  // only used if the device is on a VINT hub_port
-    }
-    if (!nh_private_.getParam("frame_id", frame_id_))
-    {
-        frame_id_ = "encoder_link";
-    }
-    if (!nh_private_.getParam("speed_filter_samples_len",
-                              speed_filter_samples_len_))
-    {
-        speed_filter_samples_len_ = 10;
-    }
-    if (!nh_private_.getParam("speed_filter_idle_iter_loops_before_reset",
-                              speed_filter_idle_iter_loops_before_reset_))
-    {
-        speed_filter_idle_iter_loops_before_reset_ = 1;
-    }
-    if (!nh_private.getParam("publish_rate", publish_rate_))
-    {
-        publish_rate_ = 0;
+        throw std::runtime_error("Publish rate must be <= 1000");
     }
 
-    ROS_INFO("Connecting to Phidgets Encoders serial %d, hub port %d ...",
-             serial_num, hub_port);
+    RCLCPP_INFO(get_logger(),
+                "Connecting to Phidgets Encoders serial %d, hub port %d ...",
+                serial_num, hub_port);
 
     // We take the mutex here and don't unlock until the end of the constructor
     // to prevent a callback from trying to use the publisher before we are
@@ -93,46 +89,46 @@ HighSpeedEncoderRosI::HighSpeedEncoderRosI(ros::NodeHandle nh,
                       std::placeholders::_3, std::placeholders::_4));
 
         n_encs = encs_->getEncoderCount();
-        ROS_INFO("Connected %d encoders", n_encs);
+        RCLCPP_INFO(get_logger(), "Connected %d encoders", n_encs);
         enc_data_to_pub_.resize(n_encs);
         for (int i = 0; i < n_encs; i++)
         {
             char str[100];
             sprintf(str, "joint%u_name", i);
-            if (!nh_private_.getParam(str, enc_data_to_pub_[i].joint_name))
-            {
-                enc_data_to_pub_[i].joint_name = "joint" + std::to_string(i);
-            }
+            enc_data_to_pub_[i].joint_name =
+                this->declare_parameter(str, "joint" + std::to_string(i));
 
             sprintf(str, "joint%u_tick2rad", i);
-            if (!nh_private_.getParam(str, enc_data_to_pub_[i].joint_tick2rad))
-            {
-                enc_data_to_pub_[i].joint_tick2rad = 1.0;
-            }
+            enc_data_to_pub_[i].joint_tick2rad =
+                this->declare_parameter(str, 1.0);
 
-            ROS_INFO("Channel %u: '%s'='%s'", i, str,
-                     enc_data_to_pub_[i].joint_name.c_str());
+            RCLCPP_INFO(get_logger(), "Channel %u: '%s'='%s'", i, str,
+                        enc_data_to_pub_[i].joint_name.c_str());
 
             char buf[100];
             sprintf(buf, "joint_states_ch%u_decim_speed", i);
-            ROS_INFO("Publishing decimated speed of channel %u to topic: %s", i,
-                     buf);
-            enc_data_to_pub_[i].encoder_decimspeed_pub =
-                nh_.advertise<phidgets_msgs::EncoderDecimatedSpeed>(buf, 10);
+            RCLCPP_INFO(get_logger(),
+                        "Publishing decimated speed of channel %u to topic: %s",
+                        i, buf);
+            enc_data_to_pub_[i].encoder_decimspeed_pub = this->create_publisher<
+                phidgets_msgs::msg::EncoderDecimatedSpeed>(buf, 10);
             encs_->setEnabled(i, true);
         }
     } catch (const Phidget22Error& err)
     {
-        ROS_ERROR("Encoders: %s", err.what());
+        RCLCPP_ERROR(get_logger(), "Encoders: %s", err.what());
         throw;
     }
 
-    encoder_pub_ = nh_.advertise<sensor_msgs::JointState>("joint_states", 100);
+    encoder_pub_ = this->create_publisher<sensor_msgs::msg::JointState>(
+        "joint_states", 100);
 
     if (publish_rate_ > 0)
     {
-        timer_ = nh_.createTimer(ros::Duration(1.0 / publish_rate_),
-                                 &HighSpeedEncoderRosI::timerCallback, this);
+        double pub_msec = 1000.0 / static_cast<double>(publish_rate_);
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(static_cast<int64_t>(pub_msec)),
+            std::bind(&HighSpeedEncoderRosI::timerCallback, this));
     } else
     {
         // If we are *not* publishing periodically, then we are event driven and
@@ -150,26 +146,26 @@ void HighSpeedEncoderRosI::publishLatest(int channel)
 {
     int64_t absolute_position = encs_->getPosition(channel);
 
-    sensor_msgs::JointState js_msg;
-    js_msg.header.stamp = ros::Time::now();
-    js_msg.header.frame_id = frame_id_;
+    auto js_msg = std::make_unique<sensor_msgs::msg::JointState>();
+    js_msg->header.stamp = this->now();
+    js_msg->header.frame_id = frame_id_;
 
-    js_msg.name.resize(enc_data_to_pub_.size());
+    js_msg->name.resize(enc_data_to_pub_.size());
     for (size_t i = 0; i < enc_data_to_pub_.size(); ++i)
     {
-        js_msg.name[i] = enc_data_to_pub_[i].joint_name;
+        js_msg->name[i] = enc_data_to_pub_[i].joint_name;
     }
 
-    js_msg.position.resize(enc_data_to_pub_.size());
-    js_msg.velocity.resize(enc_data_to_pub_.size());
-    js_msg.effort.clear();
+    js_msg->position.resize(enc_data_to_pub_.size());
+    js_msg->velocity.resize(enc_data_to_pub_.size());
+    js_msg->effort.clear();
 
     for (size_t i = 0; i < enc_data_to_pub_.size(); ++i)
     {
-        js_msg.position[i] =
+        js_msg->position[i] =
             absolute_position * enc_data_to_pub_[i].joint_tick2rad;
-        js_msg.velocity[i] = enc_data_to_pub_[i].instantaneous_speed *
-                             enc_data_to_pub_[i].joint_tick2rad;
+        js_msg->velocity[i] = enc_data_to_pub_[i].instantaneous_speed *
+                              enc_data_to_pub_[i].joint_tick2rad;
         enc_data_to_pub_[i].instantaneous_speed = 0.0;  // Reset speed
 
         if (speed_filter_samples_len_ > 0)
@@ -179,11 +175,13 @@ void HighSpeedEncoderRosI::publishLatest(int channel)
                 if (++enc_data_to_pub_[i].loops_without_update_speed_buffer >=
                     speed_filter_idle_iter_loops_before_reset_)
                 {
-                    phidgets_msgs::EncoderDecimatedSpeed e;
-                    e.header.stamp = ros::Time::now();
-                    e.header.frame_id = frame_id_;
-                    e.avr_speed = .0;
-                    enc_data_to_pub_[i].encoder_decimspeed_pub.publish(e);
+                    auto e = std::make_unique<
+                        phidgets_msgs::msg::EncoderDecimatedSpeed>();
+                    e->header.stamp = this->now();
+                    e->header.frame_id = frame_id_;
+                    e->avr_speed = .0;
+                    enc_data_to_pub_[i].encoder_decimspeed_pub->publish(
+                        std::move(e));
                 }
             } else
             {
@@ -199,20 +197,22 @@ void HighSpeedEncoderRosI::publishLatest(int channel)
                         enc_data_to_pub_[i].speeds_buffer.size();
                     enc_data_to_pub_[i].speeds_buffer.clear();
 
-                    phidgets_msgs::EncoderDecimatedSpeed e;
-                    e.header.stamp = ros::Time::now();
-                    e.header.frame_id = frame_id_;
-                    e.avr_speed = avrg * enc_data_to_pub_[i].joint_tick2rad;
-                    enc_data_to_pub_[i].encoder_decimspeed_pub.publish(e);
+                    auto e = std::make_unique<
+                        phidgets_msgs::msg::EncoderDecimatedSpeed>();
+                    e->header.stamp = this->now();
+                    e->header.frame_id = frame_id_;
+                    e->avr_speed = avrg * enc_data_to_pub_[i].joint_tick2rad;
+                    enc_data_to_pub_[i].encoder_decimspeed_pub->publish(
+                        std::move(e));
                 }
             }
         }
     }
 
-    encoder_pub_.publish(js_msg);
+    encoder_pub_->publish(std::move(js_msg));
 }
 
-void HighSpeedEncoderRosI::timerCallback(const ros::TimerEvent& /* event */)
+void HighSpeedEncoderRosI::timerCallback()
 {
     std::lock_guard<std::mutex> lock(encoder_mutex_);
     for (int i = 0; i < static_cast<int>(enc_data_to_pub_.size()); ++i)
@@ -244,3 +244,5 @@ void HighSpeedEncoderRosI::positionChangeHandler(int channel,
 }
 
 }  // namespace phidgets
+
+RCLCPP_COMPONENTS_REGISTER_NODE(phidgets::HighSpeedEncoderRosI)
