f79c72e11919492a3e440eed7e61afa1f8bce638
Michael Grupp <grupp@magazino.eu>
2021-03-09 13:26:39 +0100

    Don't publish messages that jumped back in time. (#85)
    This is basically ensuring the same behavior as it is in Melodic with
    the old sync logic.
    
    The motivation of this old PR also applies here:
    https://github.com/ros-drivers/phidgets_drivers/pull/26/

--- phidgets_gyroscope/src/gyroscope_ros_i.cpp
+++ phidgets_gyroscope/src/gyroscope_ros_i.cpp
@@ -198,8 +198,9 @@ void GyroscopeRosI::publishLatest()
 
     if (time_in_ns < last_ros_stamp_ns_)
     {
-        ROS_WARN("Time went backwards (%lu < %lu)!", time_in_ns,
-                 last_ros_stamp_ns_);
+        ROS_WARN("Time went backwards (%lu < %lu)! Not publishing message.",
+                 time_in_ns, last_ros_stamp_ns_);
+        return;
     }
 
     last_ros_stamp_ns_ = time_in_ns;
--- phidgets_spatial/src/spatial_ros_i.cpp
+++ phidgets_spatial/src/spatial_ros_i.cpp
@@ -273,8 +273,9 @@ void SpatialRosI::publishLatest()
 
     if (time_in_ns < last_ros_stamp_ns_)
     {
-        ROS_WARN("Time went backwards (%lu < %lu)!", time_in_ns,
-                 last_ros_stamp_ns_);
+        ROS_WARN("Time went backwards (%lu < %lu)! Not publishing message.",
+                 time_in_ns, last_ros_stamp_ns_);
+        return;
     }
 
     last_ros_stamp_ns_ = time_in_ns;
