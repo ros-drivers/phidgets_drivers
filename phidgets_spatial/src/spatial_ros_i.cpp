ros-drivers/phidgets_drivers

commit eaa6fa95b79d764d916bd15b0756a15e0af643a9

Rewrite IMU using libphidget22.
Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

phidgets_spatial/src/spatial_ros_i.cpp
@@ -1,0 +1,295 @@
+#include <functional>
+#include <memory>
+#include <mutex>
+
+#include <ros/ros.h>
+#include <ros/service_server.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/MagneticField.h>
+#include <std_msgs/Bool.h>
+#include <std_srvs/Empty.h>
+
+#include "phidgets_api/spatial.h"
+#include "phidgets_spatial/spatial_ros_i.h"
+
+namespace phidgets {
+
+SpatialRosI::SpatialRosI(ros::NodeHandle nh, ros::NodeHandle nh_private)
+    : nh_(nh), nh_private_(nh_private)
+{
+    ROS_INFO("Starting Phidgets SPATIAL");
+
+    ROS_INFO("Opening spatial");
+    int serial_num;
+    if (!nh_private_.getParam("serial", serial_num))
+    {
+        serial_num = -1;  // default open any device
+    }
+    int hub_port;
+    if (!nh_private.getParam("hub_port", hub_port))
+    {
+        hub_port = 0;  // only used if the device is on a VINT hub_port
+    }
+    if (!nh_private_.getParam("frame_id", frame_id_))
+    {
+        // As specified in http://www.ros.org/reps/rep-0145.html
+        frame_id_ = "imu_link";
+    }
+    if (!nh_private_.getParam("linear_acceleration_stdev",
+                              linear_acceleration_stdev_))
+    {
+        linear_acceleration_stdev_ = 300.0 * 1e-6 * G;  // 300 ug as per manual
+    }
+    if (!nh_private_.getParam("angular_velocity_stdev",
+                              angular_velocity_stdev_))
+    {
+        angular_velocity_stdev_ =
+            0.02 * (M_PI / 180.0);  // 0.02 deg/s resolution, as per manual
+    }
+    if (!nh_private_.getParam("magnetic_field_stdev", magnetic_field_stdev_))
+    {
+        magnetic_field_stdev_ =
+            0.095 * (M_PI / 180.0);  // 0.095°/s as per manual
+    }
+    int data_interval_ms;
+    if (!nh_private.getParam("data_interval_ms", data_interval_ms))
+    {
+        data_interval_ms = 250;
+    }
+    if (!nh_private.getParam("publish_rate", publish_rate_))
+    {
+        publish_rate_ = 5;
+    }
+
+    // compass correction params (see
+    // http://www.phidgets.com/docs/1044_User_Guide)
+    double cc_mag_field;
+    double cc_offset0;
+    double cc_offset1;
+    double cc_offset2;
+    double cc_gain0;
+    double cc_gain1;
+    double cc_gain2;
+    double cc_T0;
+    double cc_T1;
+    double cc_T2;
+    double cc_T3;
+    double cc_T4;
+    double cc_T5;
+
+    bool has_compass_params =
+        nh_private_.getParam("cc_mag_field", cc_mag_field) &&
+        nh_private_.getParam("cc_offset0", cc_offset0) &&
+        nh_private_.getParam("cc_offset1", cc_offset1) &&
+        nh_private_.getParam("cc_offset2", cc_offset2) &&
+        nh_private_.getParam("cc_gain0", cc_gain0) &&
+        nh_private_.getParam("cc_gain1", cc_gain1) &&
+        nh_private_.getParam("cc_gain2", cc_gain2) &&
+        nh_private_.getParam("cc_t0", cc_T0) &&
+        nh_private_.getParam("cc_t1", cc_T1) &&
+        nh_private_.getParam("cc_t2", cc_T2) &&
+        nh_private_.getParam("cc_t3", cc_T3) &&
+        nh_private_.getParam("cc_t4", cc_T4) &&
+        nh_private_.getParam("cc_t5", cc_T5);
+
+    ROS_INFO(
+        "Waiting for Phidgets Spatial serial %d, hub port %d to be "
+        "attached...",
+        serial_num, hub_port);
+
+    // We take the mutex here and don't unlock until the end of the constructor
+    // to prevent a callback from trying to use the publisher before we are
+    // finished setting up.
+    std::lock_guard<std::mutex> lock(spatial_mutex_);
+
+    spatial_ = std::make_unique<Spatial>(
+        serial_num, hub_port, false,
+        std::bind(&SpatialRosI::spatialDataCallback, this,
+                  std::placeholders::_1, std::placeholders::_2,
+                  std::placeholders::_3, std::placeholders::_4));
+
+    ROS_INFO("Connected");
+
+    spatial_->setDataInterval(data_interval_ms);
+
+    imu_pub_ = nh_.advertise<sensor_msgs::Imu>("imu/data_raw", 1);
+
+    cal_publisher_ = nh_.advertise<std_msgs::Bool>("spatial/is_calibrated", 5);
+
+    calibrate();
+
+    cal_srv_ = nh_.advertiseService("spatial/calibrate",
+                                    &SpatialRosI::calibrateService, this);
+
+    if (has_compass_params)
+    {
+        spatial_->setCompassCorrectionParameters(
+            cc_mag_field, cc_offset0, cc_offset1, cc_offset2, cc_gain0,
+            cc_gain1, cc_gain2, cc_T0, cc_T1, cc_T2, cc_T3, cc_T4, cc_T5);
+    } else
+    {
+        ROS_INFO("No compass correction params found.");
+    }
+
+    magnetic_field_pub_ =
+        nh_.advertise<sensor_msgs::MagneticField>("imu/mag", 1);
+
+    got_first_data_ = false;
+
+    if (publish_rate_ > 0)
+    {
+        timer_ = nh_.createTimer(ros::Duration(1.0 / publish_rate_),
+                                 &SpatialRosI::timerCallback, this);
+    } else
+    {
+        // We'd like to publish the latest data, but the underlying libphidget22
+        // class doesn't have the ability to grab the latest data, so we just
+        // stay silent until the first event.
+    }
+}
+
+bool SpatialRosI::calibrateService(std_srvs::Empty::Request &req,
+                                   std_srvs::Empty::Response &res)
+{
+    (void)req;
+    (void)res;
+    calibrate();
+    return true;
+}
+
+void SpatialRosI::calibrate()
+{
+    ROS_INFO("Calibrating Gyro...");
+    spatial_->zero();
+    ROS_INFO("Calibrating Gyro done.");
+
+    // publish message
+    std_msgs::Bool is_calibrated_msg;
+    is_calibrated_msg.data = true;
+    cal_publisher_.publish(is_calibrated_msg);
+}
+
+void SpatialRosI::publishLatest()
+{
+    std::shared_ptr<sensor_msgs::Imu> msg =
+        std::make_shared<sensor_msgs::Imu>();
+
+    msg->header.frame_id = frame_id_;
+
+    // build covariance matrix
+    double lin_acc_var =
+        linear_acceleration_stdev_ * linear_acceleration_stdev_;
+    double ang_vel_var = angular_velocity_stdev_ * angular_velocity_stdev_;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            int idx = j * 3 + i;
+
+            if (i == j)
+            {
+                msg->linear_acceleration_covariance[idx] = lin_acc_var;
+                msg->angular_velocity_covariance[idx] = ang_vel_var;
+            } else
+            {
+                msg->linear_acceleration_covariance[idx] = 0.0;
+                msg->angular_velocity_covariance[idx] = 0.0;
+            }
+        }
+    }
+
+    double imu_diff_in_secs = (last_data_timestamp_ - data_time_zero_) / 1000.0;
+    double time_in_secs = ros_time_zero_.toSec() + imu_diff_in_secs;
+
+    msg->header.stamp = ros::Time(time_in_secs);
+
+    // set linear acceleration
+    msg->linear_acceleration.x = -last_accel_x_ * G;
+    msg->linear_acceleration.y = -last_accel_y_ * G;
+    msg->linear_acceleration.z = -last_accel_z_ * G;
+
+    // set angular velocities
+    msg->angular_velocity.x = last_gyro_x_ * (M_PI / 180.0);
+    msg->angular_velocity.y = last_gyro_y_ * (M_PI / 180.0);
+    msg->angular_velocity.z = last_gyro_z_ * (M_PI / 180.0);
+
+    imu_pub_.publish(*msg);
+
+    std::shared_ptr<sensor_msgs::MagneticField> mag_msg =
+        std::make_shared<sensor_msgs::MagneticField>();
+
+    mag_msg->header.frame_id = frame_id_;
+
+    // build covariance matrix
+    double mag_field_var = magnetic_field_stdev_ * magnetic_field_stdev_;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            int idx = j * 3 + i;
+
+            if (i == j)
+            {
+                mag_msg->magnetic_field_covariance[idx] = mag_field_var;
+            } else
+            {
+                mag_msg->magnetic_field_covariance[idx] = 0.0;
+            }
+        }
+    }
+
+    mag_msg->header.stamp = ros::Time(time_in_secs);
+
+    // device reports data in Gauss, multiply by 1e-4 to convert to Tesla
+    mag_msg->magnetic_field.x = last_mag_x_ * 1e-4;
+    mag_msg->magnetic_field.y = last_mag_y_ * 1e-4;
+    mag_msg->magnetic_field.z = last_mag_z_ * 1e-4;
+
+    magnetic_field_pub_.publish(*mag_msg);
+}
+
+void SpatialRosI::spatialDataCallback(const double acceleration[3],
+                                      const double angular_rate[3],
+                                      const double magnetic_field[3],
+                                      double timestamp)
+{
+    std::lock_guard<std::mutex> lock(spatial_mutex_);
+    last_accel_x_ = acceleration[0];
+    last_accel_y_ = acceleration[1];
+    last_accel_z_ = acceleration[2];
+
+    last_gyro_x_ = angular_rate[0];
+    last_gyro_y_ = angular_rate[1];
+    last_gyro_z_ = angular_rate[2];
+
+    last_mag_x_ = magnetic_field[0];
+    last_mag_y_ = magnetic_field[1];
+    last_mag_z_ = magnetic_field[2];
+
+    last_data_timestamp_ = timestamp;
+
+    if (!got_first_data_)
+    {
+        got_first_data_ = true;
+        data_time_zero_ = timestamp;
+        ros_time_zero_ = ros::Time::now();
+    }
+
+    if (publish_rate_ <= 0)
+    {
+        publishLatest();
+    }
+}
+
+void SpatialRosI::timerCallback(const ros::TimerEvent & /* event */)
+{
+    std::lock_guard<std::mutex> lock(spatial_mutex_);
+    if (got_first_data_)
+    {
+        publishLatest();
+    }
+}
+
+}  // namespace phidgets

phidgets_api/CMakeLists.txt
@@ -35,12 +35,12 @@ add_library(phidgets_api src/accelerometer.cpp
                          src/digital_outputs.cpp
                          src/encoder.cpp
                          src/gyroscope.cpp
-                         src/imu.cpp
                          src/ir.cpp
                          src/magnetometer.cpp
                          src/motor.cpp
                          src/phidget21.cpp
                          src/phidget22.cpp
+                         src/spatial.cpp
                          src/temperature.cpp)
 
 add_dependencies(phidgets_api ${catkin_EXPORTED_TARGETS})

phidgets_api/include/phidgets_api/imu.h
@@ -1,40 +1,0 @@
-#ifndef PHIDGETS_API_IMU_H
-#define PHIDGETS_API_IMU_H
-
-#include "phidgets_api/phidget21.h"
-
-namespace phidgets {
-
-class Imu : public Phidget21
-{
-  public:
-    Imu();
-
-    virtual ~Imu();
-
-  protected:
-    void zero();
-    void setDataRate(int rate);
-
-    int setCompassCorrectionParameters(double cc_mag_field, double cc_offset0,
-                                       double cc_offset1, double cc_offset2,
-                                       double cc_gain0, double cc_gain1,
-                                       double cc_gain2, double cc_T0,
-                                       double cc_T1, double cc_T2, double cc_T3,
-                                       double cc_T4, double cc_T5);
-
-    virtual void dataHandler(const double acceleration[3],
-                             const double angularRate[3],
-                             const double magneticField[3], double timestamp);
-
-  private:
-    CPhidgetSpatialHandle imu_handle_;
-
-    static int SpatialDataHandler(CPhidgetSpatialHandle spatial, void *userptr,
-                                  CPhidgetSpatial_SpatialEventDataHandle *data,
-                                  int count);
-};
-
-}  // namespace phidgets
-
-#endif  // PHIDGETS_API_IMU_H

phidgets_api/include/phidgets_api/spatial.h
@@ -1,0 +1,54 @@
+#ifndef PHIDGETS_API_SPATIAL_H
+#define PHIDGETS_API_SPATIAL_H
+
+#include <functional>
+
+#include <libphidget22/phidget22.h>
+
+#include "phidgets_api/phidget22.h"
+
+namespace phidgets {
+
+class Spatial final
+{
+  public:
+    PHIDGET22_NO_COPY_NO_MOVE_NO_ASSIGN(Spatial)
+
+    explicit Spatial(int32_t serial_number, int hub_port,
+                     bool is_hub_port_device,
+                     std::function<void(const double[3], const double[3],
+                                        const double[3], double)>
+                         data_handler);
+
+    ~Spatial();
+
+    void setCompassCorrectionParameters(double cc_mag_field, double cc_offset0,
+                                        double cc_offset1, double cc_offset2,
+                                        double cc_gain0, double cc_gain1,
+                                        double cc_gain2, double cc_T0,
+                                        double cc_T1, double cc_T2,
+                                        double cc_T3, double cc_T4,
+                                        double cc_T5);
+
+    void setDataInterval(uint32_t interval_ms) const;
+
+    void zero() const;
+
+    void dataHandler(const double acceleration[3], const double angular_rate[3],
+                     const double magnetic_field[3], double timestamp) const;
+
+  private:
+    std::function<void(const double[3], const double[3], const double[3],
+                       double)>
+        data_handler_;
+    PhidgetSpatialHandle spatial_handle_;
+
+    static void DataHandler(PhidgetSpatialHandle input_handle, void *ctx,
+                            const double acceleration[3],
+                            const double angular_rate[3],
+                            const double magnetic_field[3], double timestamp);
+};
+
+}  // namespace phidgets
+
+#endif  // PHIDGETS_API_SPATIAL_H

phidgets_api/src/imu.cpp
@@ -1,74 +1,0 @@
-#include "phidgets_api/imu.h"
-
-namespace phidgets {
-
-Imu::Imu() : Phidget21(), imu_handle_(nullptr)
-{
-    // create the handle
-    CPhidgetSpatial_create(&imu_handle_);
-
-    // pass handle to base class
-    Phidget21::init((CPhidgetHandle)imu_handle_);
-
-    // register base class callbacks
-    Phidget21::registerHandlers();
-
-    // register imu data callback
-    CPhidgetSpatial_set_OnSpatialData_Handler(imu_handle_, SpatialDataHandler,
-                                              this);
-}
-
-Imu::~Imu()
-{
-}
-
-void Imu::setDataRate(int rate)
-{
-    CPhidgetSpatial_setDataRate(imu_handle_, rate);
-}
-
-void Imu::zero()
-{
-    // zero (calibrate) gyro
-    CPhidgetSpatial_zeroGyro(imu_handle_);
-}
-
-int Imu::SpatialDataHandler(CPhidgetSpatialHandle /* handle */, void *userptr,
-                            CPhidgetSpatial_SpatialEventDataHandle *data,
-                            int count)
-{
-    for (int i = 0; i < count; ++i)
-    {
-        double ts = data[i]->timestamp.seconds +
-                    (data[i]->timestamp.microseconds / 1000.0 / 1000.0);
-        ((Imu *)userptr)
-            ->dataHandler(data[i]->acceleration, data[i]->angularRate,
-                          data[i]->magneticField, ts);
-    }
-    return 0;
-}
-
-void Imu::dataHandler(const double acceleration[3], const double angularRate[3],
-                      const double magneticField[3], double timestamp)
-{
-    // This method can be overridden in a concrete subclass (e.g., ROS wrapper)
-    (void)acceleration;
-    (void)angularRate;
-    (void)magneticField;
-    (void)timestamp;
-}
-
-int Imu::setCompassCorrectionParameters(double cc_mag_field, double cc_offset0,
-                                        double cc_offset1, double cc_offset2,
-                                        double cc_gain0, double cc_gain1,
-                                        double cc_gain2, double cc_T0,
-                                        double cc_T1, double cc_T2,
-                                        double cc_T3, double cc_T4,
-                                        double cc_T5)
-{
-    return CPhidgetSpatial_setCompassCorrectionParameters(
-        imu_handle_, cc_mag_field, cc_offset0, cc_offset1, cc_offset2, cc_gain0,
-        cc_gain1, cc_gain2, cc_T0, cc_T1, cc_T2, cc_T3, cc_T4, cc_T5);
-}
-
-}  // namespace phidgets

phidgets_api/src/spatial.cpp
@@ -1,0 +1,95 @@
+#include <functional>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+#include <libphidget22/phidget22.h>
+
+#include "phidgets_api/phidget22.h"
+#include "phidgets_api/spatial.h"
+
+namespace phidgets {
+
+Spatial::Spatial(int32_t serial_number, int hub_port, bool is_hub_port_device,
+                 std::function<void(const double[3], const double[3],
+                                    const double[3], double)>
+                     data_handler)
+    : data_handler_(data_handler)
+{
+    PhidgetReturnCode ret = PhidgetSpatial_create(&spatial_handle_);
+    if (ret != EPHIDGET_OK)
+    {
+        throw Phidget22Error("Failed to create Spatial handle", ret);
+    }
+
+    helpers::openWaitForAttachment(
+        reinterpret_cast<PhidgetHandle>(spatial_handle_), serial_number,
+        hub_port, is_hub_port_device, 0);
+
+    ret = PhidgetSpatial_setOnSpatialDataHandler(spatial_handle_, DataHandler,
+                                                 this);
+    if (ret != EPHIDGET_OK)
+    {
+        throw Phidget22Error("Failed to set change handler for Spatial", ret);
+    }
+}
+
+Spatial::~Spatial()
+{
+    PhidgetHandle handle = reinterpret_cast<PhidgetHandle>(spatial_handle_);
+    helpers::closeAndDelete(&handle);
+}
+
+void Spatial::zero() const
+{
+    PhidgetReturnCode ret = PhidgetSpatial_zeroGyro(spatial_handle_);
+    if (ret != EPHIDGET_OK)
+    {
+        throw Phidget22Error("Failed to calibrate Gyroscope", ret);
+    }
+}
+
+void Spatial::setCompassCorrectionParameters(
+    double cc_mag_field, double cc_offset0, double cc_offset1,
+    double cc_offset2, double cc_gain0, double cc_gain1, double cc_gain2,
+    double cc_T0, double cc_T1, double cc_T2, double cc_T3, double cc_T4,
+    double cc_T5)
+{
+    PhidgetReturnCode ret = PhidgetSpatial_setMagnetometerCorrectionParameters(
+        spatial_handle_, cc_mag_field, cc_offset0, cc_offset1, cc_offset2,
+        cc_gain0, cc_gain1, cc_gain2, cc_T0, cc_T1, cc_T2, cc_T3, cc_T4, cc_T5);
+    if (ret != EPHIDGET_OK)
+    {
+        throw Phidget22Error("Failed to set magnetometer correction parameters",
+                             ret);
+    }
+}
+
+void Spatial::setDataInterval(uint32_t interval_ms) const
+{
+    PhidgetReturnCode ret =
+        PhidgetSpatial_setDataInterval(spatial_handle_, interval_ms);
+    if (ret != EPHIDGET_OK)
+    {
+        throw Phidget22Error("Failed to set data interval", ret);
+    }
+}
+
+void Spatial::dataHandler(const double acceleration[3],
+                          const double angular_rate[3],
+                          const double magnetic_field[3],
+                          double timestamp) const
+{
+    data_handler_(acceleration, angular_rate, magnetic_field, timestamp);
+}
+
+void Spatial::DataHandler(PhidgetSpatialHandle /* input_handle */, void *ctx,
+                          const double acceleration[3],
+                          const double angular_rate[3],
+                          const double magnetic_field[3], double timestamp)
+{
+    ((Spatial *)ctx)
+        ->dataHandler(acceleration, angular_rate, magnetic_field, timestamp);
+}
+
+}  // namespace phidgets

phidgets_drivers/package.xml
@@ -32,9 +32,9 @@
   <exec_depend>phidgets_gyroscope</exec_depend>
   <exec_depend>phidgets_ik</exec_depend>
   <exec_depend>phidgets_high_speed_encoder</exec_depend>
-  <exec_depend>phidgets_imu</exec_depend>
   <exec_depend>phidgets_magnetometer</exec_depend>
   <exec_depend>phidgets_msgs</exec_depend>
+  <exec_depend>phidgets_spatial</exec_depend>
   <exec_depend>phidgets_temperature</exec_depend>
 
   <export>

phidgets_imu/CMakeLists.txt
@@ -1,64 +1,0 @@
-cmake_minimum_required(VERSION 2.8.3)
-project(phidgets_imu)
-
-# Use C++14
-if (CMAKE_VERSION VERSION_LESS "3.1")
-    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-        set(CMAKE_CXX_FLAGS "-std=gnu++14 ${CMAKE_CXX_FLAGS}")
-    endif()
-else()
-    set (CMAKE_CXX_STANDARD 14)
-endif()
-
-# High level of warnings:
-if (CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    add_compile_options(-Wall -Wextra)
-endif()
-
-find_package(catkin REQUIRED COMPONENTS diagnostic_msgs diagnostic_updater nodelet pluginlib phidgets_api roscpp roslaunch sensor_msgs std_msgs std_srvs)
-
-catkin_package(
-    INCLUDE_DIRS include
-    LIBRARIES phidgets_imu
-    CATKIN_DEPENDS diagnostic_msgs diagnostic_updater nodelet pluginlib phidgets_api roscpp sensor_msgs std_msgs std_srvs
-)
-
-include_directories(include ${catkin_INCLUDE_DIRS})
-
-add_library(phidgets_imu src/imu_ros_i.cpp)
-add_library(phidgets_imu_nodelet src/phidgets_imu_nodelet.cpp)
-
-add_executable(phidgets_imu_node src/phidgets_imu_node.cpp)
-
-add_dependencies(phidgets_imu ${catkin_EXPORTED_TARGETS})
-add_dependencies(phidgets_imu_nodelet ${catkin_EXPORTED_TARGETS})
-add_dependencies(phidgets_imu_node ${catkin_EXPORTED_TARGETS})
-
-target_link_libraries(phidgets_imu ${catkin_LIBRARIES})
-target_link_libraries(phidgets_imu_nodelet ${catkin_LIBRARIES} phidgets_imu)
-target_link_libraries(phidgets_imu_node ${catkin_LIBRARIES} phidgets_imu)
-
-install(TARGETS phidgets_imu phidgets_imu_nodelet phidgets_imu_node
-    ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
-    LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
-    RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
-)
-
-install(DIRECTORY include/${PROJECT_NAME}/
-    DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION}
-    FILES_MATCHING PATTERN "*.h"
-)
-
-install(DIRECTORY config
-    DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
-)
-
-install(DIRECTORY launch
-    DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
-)
-
-install(FILES phidgets_imu_nodelet.xml
-    DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
-)
-
-roslaunch_add_file_check(launch)

commit 3d6110fa52ddbc4873a50d43bd217c0989838504

spatial: Fix publishing of invalid mag readings
At high data rates (e.g., the default of 250 Hz), the magnetometer
readings for every second message are invalid. The libphidget library
marks these values with PUNK_DBL (== 1e300). Because we multiply with
1e-4, this lead to the output at the end of this message.

The sensor_msgs/MagneticField message specifies that unreported readings
should be marked with "NaN". That is what this commit does.

Note that we do want to keep publishing these messages to enable a
downstream node (e.g., imu_filter_madgwick) to use a TimeSynchronizer to
synchronize between MagneticField and Imu messages.

This bug was introduced in eaa6fa95; this commit restores the behavior
to the one before that commit.

----------------------------------------------------------------------

Example output before this commit:

    header:
      seq: 1999
      stamp:
        secs: 1644940759
        nsecs: 317582657
      frame_id: "imu_link"
    magnetic_field:
      x: 4.8177e-05
      y: -2.5149e-05
      z: 1.1514000000000002e-05
    magnetic_field_covariance: [1.2100000000000002e-14, 0.0, 0.0, 0.0, 1.2100000000000002e-14, 0.0, 0.0, 0.0, 1.2100000000000002e-14]
    ---
    header:
      seq: 2000
      stamp:
        secs: 1644940759
        nsecs: 321582657
      frame_id: "imu_link"
    magnetic_field:
      x: 1.0000000000000002e+296
      y: 1.0000000000000002e+296
      z: 1.0000000000000002e+296
    magnetic_field_covariance: [1.2100000000000002e-14, 0.0, 0.0, 0.0, 1.2100000000000002e-14, 0.0, 0.0, 0.0, 1.2100000000000002e-14]
    ---
    header:
      seq: 2001
      stamp:
        secs: 1644940759
        nsecs: 325582657
      frame_id: "imu_link"
    magnetic_field:
      x: 4.8177e-05
      y: -2.5149e-05
      z: 1.1211e-05
    magnetic_field_covariance: [1.2100000000000002e-14, 0.0, 0.0, 0.0, 1.2100000000000002e-14, 0.0, 0.0, 0.0, 1.2100000000000002e-14]
    ---
    ^Cheader:
      seq: 2002
      stamp:
        secs: 1644940759

phidgets_spatial/src/spatial_ros_i.cpp
@@ -400,11 +400,27 @@ void SpatialRosI::spatialDataCallback(const double acceleration[3],
         last_gyro_y_ = angular_rate[1] * (M_PI / 180.0);
         last_gyro_z_ = angular_rate[2] * (M_PI / 180.0);
 
-        // device reports data in Gauss, multiply by 1e-4 to convert to Tesla
-        last_mag_x_ = magnetic_field[0] * 1e-4;
-        last_mag_y_ = magnetic_field[1] * 1e-4;
-        last_mag_z_ = magnetic_field[2] * 1e-4;
+        if (magnetic_field[0] != PUNK_DBL)
+        {
+            // device reports data in Gauss, multiply by 1e-4 to convert to
+            // Tesla
+            last_mag_x_ = magnetic_field[0] * 1e-4;
+            last_mag_y_ = magnetic_field[1] * 1e-4;
+            last_mag_z_ = magnetic_field[2] * 1e-4;
+        } else
+        {
+            // data is PUNK_DBL ("unknown double"), which means the magnetometer
+            // did not return valid readings. When publishing at 250 Hz, this
+            // will happen in every second message, because the magnetometer can
+            // only sample at 125 Hz. It is still important to publish these
+            // messages, because a downstream node sometimes uses a
+            // TimeSynchronizer to get Imu and Magnetometer nodes.
+            double nan = std::numeric_limits<double>::quiet_NaN();
 
+            last_mag_x_ = nan;
+            last_mag_y_ = nan;
+            last_mag_z_ = nan;
+        }
         last_data_timestamp_ns_ = this_ts_ns;
 
         // Publish if we aren't publishing on a timer